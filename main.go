ddb51a31999bf70c8756b6497339ddc9203d9acc
Pallavee980kri <pallavee.kumari@razorpay.com>
2023-06-21 13:59:49 +0530

    working_on_verify_certificate

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main (){
-	
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	cert, error := tls.LoadX509KeyPair("./certificate.crt", "./private.key")
+	if error != nil {
+		panic(error)
+	}
+
+	customtlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: customtlsConfig,
+		},
+	}
+
+	hittingURL := "https://apideveloper.rblbank.com/test/sb/rbl/api/v1/sendernotification/rpay"
+
+	response, error := httpClient.Get(hittingURL)
+	if error != nil {
+		panic(error)
+	}
+
+	defer response.Body.Close()
+
+	responseBody, error := ioutil.ReadAll(response.Body)
+
+	if error != nil {
+		panic(error)
+	}
+	fmt.Println(string(responseBody))
 }
